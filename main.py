d909327244128c7ae449c12c655ed4b4e4371136
Eurisko-H <fitthasan@gmail.com>
2022-04-27 04:37:48 +0300

    Create a function to check the input
    To make sure that the user input is only a number and between 1-9

--- main.py
+++ main.py
@@ -12,12 +12,23 @@ def game_board(main_board):
         print()
 
 
-game_board(board)
+def quit(user_input):
+    if user_input.lower() == 'q':
+        return True
 
 
-
+def check_input(user_input):
+    try:
+        val = int(user_input)
+        if 9 >= val >= 1:
+            return True
+        print("remember that the input is between 1 and 9")
+    except ValueError:
+        print("That's not an int!")
 
 
 while True:
     game_board(board)
     user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit: ")
+    if quit(user_input): break
+    if not check_input(user_input): continue
